64949e90f4eff30d58df51caade5d66c19ea4429
ngocphuock16 <ngocphuoc8798@gmail.com>
2021-01-09 14:33:55 +0700

    bai6

---
+++ Bai6 Tìm phần tử lớn nhất của List gồm các số.py
@@ -0,0 +1,10 @@
+import random
+
+n = int(input('Nhap n :'))
+list = random.sample(range(0,100),n)
+max = list[0]
+for item in list:
+    if item > max :
+        max = item
+print('List = ',list)
+print('Gia tri lon nhat cua list la :',max)
